133045cc967eccfbcc102e648f5575c8f09a8792
chrscchrn <cochranschris@gmail.com>
2021-01-06 21:13:22 -0600

    manifest and icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,5 +39,26 @@ module.exports = {
         cssPropOptimization: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Lauren's Art`,
+        short_name: `Yarr`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/favicon-32x32.png`, // This path is relative to the root of the site.
+        icons: [
+          {
+            src: `/images/favicon-16x16.png`,
+            sizes: `16x16`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
   ],
 };
